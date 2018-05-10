1b4021c6377a6b3bf038165ddc651c639fc30ceb
bbukaty <buck.bukaty@gmail.com>
2018-05-10 16:58:53 -0700

    input queue instead of interval + key presses, next buffer+key ups

--- mss_test.py
+++ mss_test.py
@@ -3,6 +3,7 @@ import threading
 import pythoncom, pyHook 
 import numpy as np
 import win32gui
+from Queue import Queue
 
 import mss
 import mss.tools
@@ -10,16 +11,17 @@ import mss.tools
 # from PIL import ImageGrab
 
 class InputCapture:
-    def __init__(self, gameWindowName="Crypt of the NecroDancer", dt=1/10.0):
+    def __init__(self, dt, gameWindowName="Crypt of the NecroDancer"):
         self.gameWindowName = gameWindowName
         self.dt = dt
 
-        self.actionKeys = ["Down","Up","Right","Left"]
         self.capNumber = 0
         self.capsFolder = "caps/testing/"
         self.isCapturing = True
-
         self.sct = mss.mss()
+
+        self.actionKeys = ["Down","Up","Right","Left"]
+        self.inputQueue = Queue()
 
         gameWindow = win32gui.FindWindow(None, self.gameWindowName)
         if gameWindow == 0:
@@ -27,11 +29,10 @@ class InputCapture:
             exit()
         win32gui.SetForegroundWindow(gameWindow)
         bbox = win32gui.GetWindowRect(gameWindow)
-        print(bbox)
+
         width = bbox[2]-bbox[0]
         height = bbox[3]-bbox[1]
         self.gameBbox = {'top': bbox[1], 'left': bbox[0], 'width': width, 'height': height}
-        print self.gameBbox
 
         self.captureThread = threading.Thread(target=self.captureFrames)
         
@@ -50,23 +51,25 @@ class InputCapture:
     def captureFrames(self):
         # TODO: see if using windows api calls is faster https://www.quora.com/How-can-we-take-screenshots-using-Python-in-Windows
         while self.isCapturing:
-            try:
-                tic = time.time()
-                # Grab the data
-                sct_img = self.sct.grab(self.gameBbox)
-                # Save to the picture file
-                fileName = self.capsFolder + str(self.capNumber) + ".png"
-                mss.tools.to_png(sct_img.rgb, sct_img.size, output=fileName)
+            tic = time.time()
+            actionCombo = ""
+            while not self.inputQueue.empty():
+                actionCombo += self.inputQueue.get()
+            
+            # Grab the data
+            sct_img = self.sct.grab(self.gameBbox)
+            # Save to the picture file
+            fileName = self.capsFolder + str(self.capNumber) + actionCombo + ".png"
+            mss.tools.to_png(sct_img.rgb, sct_img.size, output=fileName)
 
-                self.capNumber += 1
+            self.capNumber += 1
 
-                sleepAmount = self.dt-(time.time()-tic)
-                if sleepAmount < 0:
-                    print 'Lagging, missed frame by {}'.format(-sleepAmount)
-                    sleepAmount = 0
-                time.sleep(sleepAmount)
-            except IOError:
-                pass
+            sleepAmount = self.dt-(time.time()-tic)
+            if sleepAmount < 0:
+                print 'Lagging, missed frame by {}'.format(-sleepAmount)
+                sleepAmount = 0
+            time.sleep(sleepAmount)
+
 
 
     def onKeyboardEvent(self, event):
@@ -78,13 +81,8 @@ class InputCapture:
         # print '---'
         # print event.Key
         if event.Key in self.actionKeys:
-            # Grab the data
-            sct_img = self.sct.grab(self.gameBbox)
-            # Save to the picture file
-            fileName = self.capsFolder + str(self.capNumber) + event.Key + ".png"
-            mss.tools.to_png(sct_img.rgb, sct_img.size, output=fileName)
+            self.inputQueue.put('_'+event.Key)
             # print('KeyboardEvent: {}'.format(self.capNumber))
-            self.capNumber += 1
         elif event.Key == "Escape":
             self.isCapturing = False
             print('Waiting for thread...')
@@ -99,4 +97,4 @@ class InputCapture:
 
 
 if __name__ == '__main__':
-    inputCapture = InputCapture(dt=2.0)
+    inputCapture = InputCapture(1/2.0)
